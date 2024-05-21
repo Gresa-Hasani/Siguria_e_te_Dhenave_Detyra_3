9cd90c4c0f23a13c7d4a76941403a3c78662e8c9
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-05-21 19:32:00 +0200

    Updated Client

--- src/main/java/DetyraETrete/Client.java
+++ src/main/java/DetyraETrete/Client.java
@@ -1,4 +1,42 @@
-byte[] sharedSecret = CryptoUtils.generateSharedSecret(clientKeyPair.getPrivate(), serverPublicKey);
+package DetyraETrete;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyPair;
+import java.security.PublicKey;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class Client {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 8080;
+    private static KeyPair clientKeyPair;
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static void main(String[] args) throws Exception {
+        clientKeyPair = CryptoUtils.generateKeyPair();
+
+        try (Socket serverSocket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(serverSocket.getOutputStream(), true);
+             BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Welcome to Simplified SSH Client.");
+            System.out.println("Attempting to connect to the SSH server...");
+
+            String serverPubKeyStr = in.readLine();
+            PublicKey serverPublicKey = CryptoUtils.getPublicKeyFromString(serverPubKeyStr);
+
+            out.println(Base64.getEncoder().encodeToString(clientKeyPair.getPublic().getEncoded()));
+            byte[] sharedSecret = CryptoUtils.generateSharedSecret(clientKeyPair.getPrivate(), serverPublicKey);
             
 boolean isVerified = CryptoUtils.verifySignature(serverPublicKey, "Server Authentication", signature);
         if (!isVerified) {
