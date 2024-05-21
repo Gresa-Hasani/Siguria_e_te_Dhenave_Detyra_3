6c49b4aded31f75cbdabc8a3ff03795251806fd7
Gzon Alaj <155363033+gzoni@users.noreply.github.com>
2024-05-21 19:52:34 +0200

    Update Client.java

--- src/main/java/DetyraETrete/Client.java
+++ src/main/java/DetyraETrete/Client.java
@@ -37,3 +37,18 @@ public class Client {
             PublicKey serverPublicKey = CryptoUtils.getPublicKeyFromString(serverPubKeyStr);
 
             out.println(Base64.getEncoder().encodeToString(clientKeyPair.getPublic().getEncoded()));
+
+                        byte[] sharedSecret = CryptoUtils.generateSharedSecret(clientKeyPair.getPrivate(), serverPublicKey);
+            
+            boolean isVerified = CryptoUtils.verifySignature(serverPublicKey, "Server Authentication", signature);
+            if (!isVerified) {
+                System.out.println("Server verification failed. Exiting...");
+                return;
+            }
+
+            String hmac = in.readLine();
+            String expectedHmac = CryptoUtils.generateHMAC("Server Authentication", sharedSecret);
+            if (!hmac.equals(expectedHmac)) {
+                System.out.println("HMAC verification failed. Exiting...");
+                return;
+            }
