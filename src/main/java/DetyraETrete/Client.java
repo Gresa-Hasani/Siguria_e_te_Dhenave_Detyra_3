1bdf88015cff11cbbf9c0511a0de920be9a06223
Gzon Alaj <155363033+gzoni@users.noreply.github.com>
2024-05-21 18:05:46 +0200

    Update Client.java

--- src/main/java/DetyraETrete/Client.java
+++ src/main/java/DetyraETrete/Client.java
@@ -1,1 +1,14 @@
+byte[] sharedSecret = CryptoUtils.generateSharedSecret(clientKeyPair.getPrivate(), serverPublicKey);
+            
+boolean isVerified = CryptoUtils.verifySignature(serverPublicKey, "Server Authentication", signature);
+        if (!isVerified) {
+            System.out.println("Server verification failed. Exiting...");
+            return;
+            }
 
+String hmac = in.readLine();
+String expectedHmac = CryptoUtils.generateHMAC("Server Authentication", sharedSecret);
+        if (!hmac.equals(expectedHmac)) {
+            System.out.println("HMAC verification failed. Exiting...");
+            return;
+            }
